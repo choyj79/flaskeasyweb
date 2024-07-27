fa292afac1915d59470f7111b8687b3c14f32cee
choyj <jojju486@gmail.com>
2024-07-27 10:52:52 +0900

    1 commit

---
+++ apps/easyapp/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Hello, 성공!!!'
+
+@app.route("/hello/<name>",methods=["GET"],endpoint='hello-enepoint')
+def hello(name):
+    return f"hello!! {name}!!"
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html",name=name)
+
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("show_name",name='choyj',page = 1))
