0ba023c81b507522be50f4d3d995ef6ca2691925
choyj <jojju486@gmail.com>
2024-08-03 10:54:28 +0900

    240803 cimmit

--- apps/easyapp/app.py
+++ apps/easyapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,22 @@ def hello(name):
 @app.route("/name/<name>")
 def show_name(name):
     return render_template("index.html",name=name)
+## 문의 폼############
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+##문의 완료 폼##############
+@app.route("/contact_complete", methods=["GET","POST"])
+def contact_complete():
+    if request.method == "POST":
+        #html의 form 속성에서 값을 가져오는 부분
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+        
+        return redirect(url_for("contact_complete"))
+    return render_template("contact_complete.html")
 
 with app.test_request_context():
     print(url_for("index"))
