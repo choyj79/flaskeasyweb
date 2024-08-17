3b00db888ec920f491b21a326a47499920eeab9f
choyj <jojju486@gmail.com>
2024-08-17 11:08:11 +0900

    240817 commit

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from pathlib import Path
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    #앱의 config 설정
+    app.config.from_mapping(
+            SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ",
+            SQLALCHEMY_DATABASE_URI =f"sqlite:///{Path(__file__).parent.parent/'local.sqlite'}", 
+            SQLALCHEMY_TRACK_MODIFICATIONS=False
+            )     
+
+    # SQLAlchemy와 앱을 연계한
+    db.init_app(app) 
+    # Migrate와 앱을 연계한다
+    Migrate(app, db)
 
     from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud,url_prefix="/crud") 
